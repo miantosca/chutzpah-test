0079c7048b7f6b9ef12a83b60ace68ced78f1ccc
Michael Iantosca <Michael Iantosca>
2016-06-02 14:08:17 -0400

    initial commit

---
+++ src/median.ts
@@ -0,0 +1,22 @@
+﻿export class median {
+
+    calculate(dataValues): number {
+
+        let sortedDataValues = dataValues.map((dataPoint) => {
+            return parseFloat(dataPoint.calculatedValue);
+        }).sort((a, b) => { return a - b; });    
+
+        return (sortedDataValues.length % 2) === 0 ?
+            this.evenLengthMedian(sortedDataValues) : this.oddLengthMedian(sortedDataValues);
+    }
+
+    private evenLengthMedian(sortedDataValues): number {
+        let firstPointIndex = (sortedDataValues.length / 2) - 1 ;
+        return (sortedDataValues[firstPointIndex] + sortedDataValues[firstPointIndex + 1]) / 2 ;
+    }
+
+    private oddLengthMedian(sortedDataValues): number {
+        let middleIndex = (sortedDataValues.length + 1 ) / 2 - 1;
+        return sortedDataValues[middleIndex];
+    }
+}
---
+++ test/tests.ts
@@ -0,0 +1,20 @@
+﻿import {median} from '../src/median';
+
+let rc = new median();
+
+
+describe("A suite", function () { 
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });  
+    
+}); 
+
+describe("A suite is just a function", function () {
+    var a;
+
+    it("and so is a spec", function () {
+        a = true;
+        expect(a).toBe(true);
+    });
+});
